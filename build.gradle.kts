e918f6413bce049b1081e15ce4d5401fd05d268e
Hiroshi Miura <miurahr@linux.com>
2020-12-26 13:05:18 +0900

    Use Kotlin DSL for gradle
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+    groovy
+    `groovy-gradle-plugin`
+    id("com.gradle.plugin-publish") version "0.12.0"
+    `maven-publish`
+}
+group = "org.omegat"
+version = "1.2.3"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(gradleApi())
+    implementation(localGroovy())
+}
+
+pluginBundle {
+    vcsUrl = "https://github.com/miurahr/gradle-omegat.git"
+    website = "https://github.com/miurahr/gradle-omegat"
+    description = "OmegaT plugin for Gradle"
+    tags = listOf("OmegaT")
+    plugins.create("omegatPlugin") {
+        id = "org.omegat.gradle"
+        displayName = "OmegaT plugin"
+    }
+}
+
+gradlePlugin {
+    plugins.create("omegatPlugin") {
+        id = "org.omegat.gradle"
+        implementationClass = "org.omegat.gradle.OmegatPlugin"
+    }
+}
+publishing {
+  publications.withType(MavenPublication::class).all {
+    if (name == "pluginMaven") {
+      artifactId = "gradle-omegat-plugin"
+    }
+  }
+}
