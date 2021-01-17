2215d3e56339cbdea4b1e3e0fb9d62de0e8caf1f
Hiroshi Miura <miurahr@linux.com>
2021-01-18 07:45:37 +0900

    Allow dabble-quote for plugin.link property
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/kotlin/org/omegat/gradle/config/OmegatManifest.kt
+++ src/main/kotlin/org/omegat/gradle/config/OmegatManifest.kt
@@ -17,7 +17,9 @@ class OmegatManifest(private val project: Project, private val extension: Plugin
     var author: String? = project.findProperty("plugin.author")?.toString()
     var description: String? = project.findProperty("plugin.description")?.toString()
     var website: URL? =
-        if (project.hasProperty("plugin.link")) URL(project.findProperty("plugin.link").toString()) else null
+        if (project.hasProperty("plugin.link"))
+            URL(project.findProperty("plugin.link").toString().removeSurrounding("\""))
+        else null
 
     fun createOmegatPluginJarManifest(): Map<String, String> {
 
@@ -27,7 +29,7 @@ class OmegatManifest(private val project: Project, private val extension: Plugin
             Attribute.PLUGIN_MAIN_CLASS to extension.pluginClass,
             Attribute.PLUGIN_DESCRIPTION to description,
             Attribute.PLUGIN_VERSION to project.version.toString(),
-            Attribute.PLUGIN_WEBSITE to website?.toString()
+        Attribute.PLUGIN_WEBSITE to website?.toString()
         )
         return manifestAtts.mapNotNull { entry ->
           entry.value?.let { Pair(entry.key, it) } // only return entries with not-null value
