09f80df17d9d45b675d1f54c063037ae596d0185
Hiroshi Miura <miurahr@linux.com>
2022-01-09 23:08:28 +0900

    Update PluginExtension
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/main/kotlin/org/omegat/gradle/config/PluginExtension.kt
+++ src/main/kotlin/org/omegat/gradle/config/PluginExtension.kt
@@ -2,16 +2,11 @@ package org.omegat.gradle.config
 
 import groovy.lang.Closure
 import org.gradle.api.Project
-import org.gradle.api.plugins.BasePluginConvention
 import org.gradle.api.tasks.util.PatternFilterable
 
 
 open class PluginExtension(val project: Project) {
-    var pluginName: String
-    get() = project.convention.getPlugin(BasePluginConvention::class.java).archivesBaseName
-    set(value) {
-      project.convention.getPlugin(BasePluginConvention::class.java).archivesBaseName = value
-    }
+    var pluginName: String? = null
     var projectDir: String? = null
     var version: String? = null
     var pluginClass: String? = null
