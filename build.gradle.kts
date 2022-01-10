0772a76303c240c2ad2421f47b348a060ca30f77
Hiroshi Miura <miurahr@linux.com>
2022-01-10 12:17:50 +0900

    Release v1.5.5
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     kotlin("jvm") version "1.6.10"
     `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "0.19.0"
-    `maven-publish`
+    id("com.gradle.plugin-publish") version "0.19.0"  // for publish to plugin portal
+    `maven-publish`  // for publish to mavenLocal
 }
+
 group = "org.omegat"
-version = "1.5.4"
+version = "1.5.5"
 
 tasks.compileJava {
     options.release.set(8)
@@ -24,7 +25,7 @@ pluginBundle {
     vcsUrl = "https://github.com/miurahr/gradle-omegat.git"
     website = "https://github.com/miurahr/gradle-omegat-plugin"
     description = "OmegaT plugin for Gradle"
-    tags = listOf("OmegaT")
+    tags = listOf("OmegaT", "translation", "plugin development")
     plugins.create("omegatPlugin") {
         id = "org.omegat.gradle"
         displayName = "OmegaT plugin"
